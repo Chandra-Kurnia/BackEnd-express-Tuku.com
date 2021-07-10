c3d5bcb03930bf3eba2685f26de901c9d2df2f54
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-07-10 15:40:41 +0700

    create + update  .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
+require('dotenv').config()
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'taskNode',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   });
   
   module.exports = conn
